b48fb5aa56bb2ee81253850d875e24f70ffd0282
NicolasV <nsvanegasg@udistrital.edu.co>
2026-02-28 20:02:25 -0500

    Agrego funcion sonAdyacentes en MovimientoLogic.kt

--- app/src/main/java/com/example/taller1/MovimientoLogic.kt
+++ app/src/main/java/com/example/taller1/MovimientoLogic.kt
@@ -7,3 +7,19 @@ fun indiceToPosicion(indice: Int): Pair<Int, Int>{
     return Pair(fila,columna)
 
 }
+
+fun sonAdyacentes(indice1: Int, indice2: Int): Boolean {
+
+    val posicion1 =indiceToPosicion(indice1)
+    val posicion2 =indiceToPosicion(indice2)
+
+    val mismaFila = posicion1.first == posicion2.first
+    val mismaColumna = posicion1.second == posicion2.second
+
+    val filaDiferencia = Math.abs(posicion1.first - posicion2.first)
+    val columnaDiferencia = Math.abs(posicion1.second - posicion2.second)
+
+    return(mismaFila && columnaDiferencia == 1) || (mismaColumna && filaDiferencia == 1)
+}
+
+
